8900933ce89e6325a6c1537e9a87e1dff2864185
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2022-06-09 12:02:07 +0530

    Update letschathome.js

---
+++ letschathome.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAG-Mq72qFU68ozPKEZdvmv_Fe3M4eThM",
+  authDomain: "webchat-database.firebaseapp.com",
+  databaseURL: "https://webchat-database-default-rtdb.firebaseio.com",
+  projectId: "webchat-database",
+  storageBucket: "webchat-database.appspot.com",
+  messagingSenderId: "321090356994",
+  appId: "1:321090356994:web:7d76e26b92c2465fcc641b"
+};
+
+firebase.initializeApp(firebaseConfig);
